9ea998e09b89d7895e14026cbe0e189011e2e225
lololol <lololol@lololols-House-2.local>
2015-06-18 18:26:39 +0800

    Initial Commit

---
+++ testWriteToPlist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testWriteToPlist
+//
+//  Created by lololol on 18/Jun/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testWriteToPlist/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testWriteToPlist
+//
+//  Created by lololol on 18/Jun/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
